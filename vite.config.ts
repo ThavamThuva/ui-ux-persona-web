cbd8ae49eee88cce57156fdc1c8bb906765530b3
Thuvarakan Thavarasa <96078916+ThavamThuva@users.noreply.github.com>
2025-06-08 00:14:00 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,20 +3,26 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+// Replace 'your-repo-name' with your actual GitHub repository name
+export default defineConfig({
+  base: '/your-repo-name/',
+  plugins: [react()],
+})
+
+// // https://vitejs.dev/config/
+// export default defineConfig(({ mode }) => ({
+//   server: {
+//     host: "::",
+//     port: 8080,
+//   },
+//   plugins: [
+//     react(),
+//     mode === 'development' &&
+//     componentTagger(),
+//   ].filter(Boolean),
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// }));
