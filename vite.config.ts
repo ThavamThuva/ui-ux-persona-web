47abc126d28900988d225e9a930d5ee1eaf0a48c
Thuvarakan Thavarasa <96078916+ThavamThuva@users.noreply.github.com>
2025-06-08 07:03:58 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,23 +3,26 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-const repoName = "ui-ux-persona-web";
+export default defineConfig({
+  plugins: [react()],
+  base = "/ui-ux-persona-web",
+});
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: `/${repoName}/`,
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+// // https://vitejs.dev/config/
+// export default defineConfig(({ mode }) => ({
+//   base: `/${repoName}/`,
+//   server: {
+//     host: "::",
+//     port: 8080,
+//   },
+//   plugins: [
+//     react(),
+//     mode === 'development' &&
+//     componentTagger(),
+//   ].filter(Boolean),
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// }));
